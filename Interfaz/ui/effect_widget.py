8209a2f04b6fec9649658b8eea6895153f2efd7d
Samir <samirbarce@gmail.com>
2026-02-28 20:27:53 -0500

    Added parameters + drags

---
+++ Interfaz/ui/effect_widget.py
@@ -0,0 +1,58 @@
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel, QSlider
+from PyQt6.QtCore import Qt
+
+class EffectWidget(QWidget):
+    def __init__(self, effect_data, update_callback):
+        super().__init__()
+        
+        self.effect_data = effect_data
+        self.update_callback = update_callback
+        self.expanded = False
+
+        self.main_layout = QVBoxLayout()
+        self.setLayout(self.main_layout)
+        
+        #Header
+        self.header_button = QPushButton(effect_data["type"] + " ▼")
+        self.header_button.setCheckable(True)
+        self.header_button.clicked.connect(self.toggle_expand)
+        self.main_layout.addWidget(self.header_button)
+
+        #Parametros
+        self.params_Widget = QWidget()
+        self.params_Layout = QVBoxLayout()
+        self.params_Widget.setLayout(self.params_Layout)
+        self.sliders = {}
+
+        #Sliders
+        for param, value in effect_data["params"].items():
+            label = QLabel(f"{param}: {value}")
+            slider = QSlider(Qt.Orientation.Horizontal)
+            slider.setMinimum(0)
+            slider.setMaximum(100)
+            slider.setValue(int(value * 100))
+
+            slider.valueChanged.connect(
+                lambda val, p=param, l=label: self.update_param(p, val, l)
+                
+            )
+            self.params_Layout.addWidget(label)
+            self.params_Layout.addWidget(slider)
+            self.sliders[param] = slider
+        
+        self.main_layout.addWidget(self.params_Widget)
+        self.params_Widget.setVisible(False)
+
+    def toggle_expand(self):
+        self.expanded = not self.expanded
+        self.params_Widget.setVisible(self.expanded)
+
+        arrow = "▲" if self.expanded else "▼" 
+        self.header_button.setText(self.effect_data["type"] + " " + arrow)
+
+    def update_param(self, param, value, label):
+        normalized = value / 100
+        self.effect_data["params"][param] = normalized
+        label.setText(f"{param}: {round(normalized, 2)}") 
+
+        self.update_callback()
---
+++ Interfaz/ui/main_window.py
@@ -0,0 +1,159 @@
+import sys
+import numpy as np
+import pyqtgraph as pg
+import json
+
+from ui.effect_widget import EffectWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QSlider, QLabel, QComboBox, QListWidget, QListWidgetItem 
+from PyQt6.QtCore import QTimer, Qt
+
+class MainWindow(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Audio Interface")
+
+        self.t = 0
+        self.effect_on = False
+        self.drive = 1
+        
+        #Definimos el tipo de layer
+        self.main_layout = QHBoxLayout()
+        self.setLayout(self.main_layout)
+
+        self.left_layout = QVBoxLayout()
+
+        #Titulo
+        self.title_label = QLabel("MultiFX Processor")
+        self.left_layout.addWidget(self.title_label)
+        
+        #Dropdown
+        self.preset_dropdown = QComboBox()
+        self.preset_dropdown.addItems(["Preset 1", "Preset 2", "Preset 3"])
+        self.left_layout.addWidget(self.preset_dropdown)
+        
+        #Lista de efectos
+        self.effects_list = QListWidget()
+        self.effects_list.setDragDropMode(QListWidget.DragDropMode.InternalMove)
+        self.effects_list.model().rowsMoved.connect(self.update_effect_order)
+        self.effects = [
+            {"type": "Distortion", "enabled": True, "params": {"GAIN": 0.5, "TONE": 0.5, "OUTPUT": 0.5}},
+            {"type": "Delay", "enabled": True, "params": {"TIME": 0.5, "FEEDBACK": 0.3, "MIX": 0.2}},
+            {"type": "Wah", "enabled": True, "params": {}},
+            {"type": "Flanger", "enabled": True, "params": {}},
+        ]
+        self.preset_name = "Preset1"
+
+        #Cargar efectos
+        self.load_effects()
+        self.left_layout.addWidget(self.effects_list)
+        
+        self.right_layout = QVBoxLayout()
+
+        self.main_layout.addLayout(self.left_layout, 1)
+        self.main_layout.addLayout(self.right_layout, 2)
+
+        #Boton
+        self.button = QPushButton("Toggle Effect")
+        self.left_layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.toggle_effect)
+
+        #Labels
+        self.drive_label = QLabel("Drive: 1")
+        self.left_layout.addWidget(self.drive_label)
+
+        #Slider
+        self.drive_slider = QSlider(Qt.Orientation.Horizontal)
+        self.drive_slider.setMinimum(1)
+        self.drive_slider.setMaximum(20)    
+        self.drive_slider.setValue(1)
+        self.left_layout.addWidget(self.drive_slider)
+
+        self.drive_slider.valueChanged.connect(self.update_drive)
+
+        
+
+        #Plot de las señales
+        self.plot_pre = pg.PlotWidget(title="Pre Effect Signal")
+        self.plot_pre.setTitle("Pre Effect", size="12pt")
+        self.plot_pre.setLabel("left", "Amplitude")
+        self.plot_pre.setLabel("bottom", "Time")
+        self.curve_pre = self.plot_pre.plot()
+        self.right_layout.addWidget(self.plot_pre)
+
+        self.plot_post = pg.PlotWidget(title="Post Effect Signal")
+        self.plot_post.setTitle("Post Effect", size="12pt")
+        self.plot_post.setLabel("left", "Amplitude")
+        self.plot_post.setLabel("bottom", "Time")
+        self.curve_post = self.plot_post.plot()
+        self.right_layout.addWidget(self.plot_post)
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.sim_signal)
+        self.timer.start(80) #Elegir velocidad en la que se generan los puntos
+
+        
+    def update_effect_order(self, *args):
+        new_order = []
+
+        for i in range(self.effects_list.count()):
+            item = self.effects_list.item(i)
+            widget = self.effects_list.itemWidget(item)
+            new_order.append(widget.effect_data)
+
+        self.effects = new_order
+        print("Order updated")
+        self.generate_json()
+        
+    #Presionar efecto
+    def toggle_effect(self):
+        self.effect_on = not self.effect_on
+        self.effects[0]["enabled"] = self.effect_on
+        print("Effect:", self.effect_on)
+        self.generate_json()
+    
+    #Cargar efectos
+    def load_effects(self):
+        self.effects_list.clear()
+        
+        for effect in self.effects:
+            item = QListWidgetItem()
+            widget = EffectWidget(effect, self.generate_json)
+            
+            item.setSizeHint(widget.sizeHint())
+
+            self.effects_list.addItem(item)
+            self.effects_list.setItemWidget(item, widget)
+    
+    #Generación del Json
+    def generate_json(self):
+        payload = {
+            "command": "apply_preset",
+            "name": self.preset_name,
+            "effects": self.effects   
+        }
+        print("JSON ready for C++: ")
+        print(json.dumps(payload, indent=2))
+
+    #Print de valor del Drive
+    def update_drive(self, value):
+        self.drive = value
+        self.drive_label.setText(f"Drive: {value}")
+
+    #Definir funciones
+    def sim_signal(self):
+        x = np.linspace(self.t, self.t + 1, 500)
+        
+        clean_sig = np.sin(2 * np.pi * 5 * x)
+
+        if self.effect_on:
+            processed_sig = np.tanh(clean_sig * self.drive)
+        else:
+            processed_sig = clean_sig.copy()
+
+        self.curve_pre.setData(clean_sig)
+        self.curve_post.setData(processed_sig)
+
+        self.t += 0.05
