93515bd261aeadb96bf8e290b52be8a6d155c5fd
Samir Barceló <162069964+SamirUwu@users.noreply.github.com>
2026-03-04 17:03:47 -0500

    Create run_all_sh.py

---
+++ run_all_sh.py
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Limpiando procesos viejos..."
+pkill -f main      # tu servidor C
+pkill -f main.py   # la interfaz Python
+sleep 0.5
+
+# Ejecutable C (socket server)
+cd ~/audio_rpi
+echo "Iniciando servidor C..."
+./main &    # ejecutable de C
+sleep 0.5   # espera a que el socket se cree
+
+# Activar virtual environment y correr Python GUI
+cd ~/Interfaz
+echo "Activando virtual environment..."
+source env/bin/activate   # o venv/bin/activate si usas venv
+
+echo "Iniciando interfaz..."
+python main.py &
+
+# Mantener el script activo mientras corren todos
+wait
