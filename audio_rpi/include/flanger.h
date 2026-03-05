89a9c3390b1d3bd25b6b62413a6efb70cf655ba3
Samir Barceló <162069964+SamirUwu@users.noreply.github.com>
2026-03-05 16:56:59 -0500

    Update flanger.h

--- audio_rpi/include/flanger.h
+++ audio_rpi/include/flanger.h
@@ -1,1 +1,23 @@
+#ifndef FLANGER_H
+#define FLANGER_H
 
+#define SAMPLE_RATE 41100
+
+#ifndef PI
+#define PI 3.14159265358979323846f
+#endif
+
+typedef struct {
+    float rate;    
+    float depth;    
+    float feedback;
+    flaot mix;
+} Flanger;
+
+// Inicializa el efecto
+void Flanger_init(Flanger *flanger, float rate, float depth, float feedback, float mix);
+
+// Procesa una sola muestra
+float Flanger_process(Wah *flanger, float input);
+
+#endif
