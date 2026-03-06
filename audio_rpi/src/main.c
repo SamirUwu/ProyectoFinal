fc1ae09ce3c899e2c65df4843d01b7b624e31c38
Samir <samirbarce@gmail.com>
2026-03-05 20:52:17 -0500

    more changes

--- audio_rpi/src/main.c
+++ audio_rpi/src/main.c
@@ -33,8 +33,9 @@ int main()
 
     for (int i = 0; i < SAMPLE_RATE; i++)
     {
-        float input = sinf(2.0f * PI * 440.0f * i / SAMPLE_RATE);
-
+        float input = 0.5f * sinf(2.0f * PI * 440.0f * i / SAMPLE_RATE)     // fundamental
+                    + 0.3f * sinf(2.0f * PI * 880.0f * i / SAMPLE_RATE)     // 2do armónico
+                    + 0.2f * sinf(2.0f * PI * 1320.0f * i / SAMPLE_RATE);   // 3er armónico
         //float od_out = Overdrive_process(&od, input);
         //float ch_out = Chorus_process(&ch, input);
 
