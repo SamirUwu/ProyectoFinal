067b326ac9c017f0227a409bb9f1ae2543dce73c
Samir <samirbarce@gmail.com>
2026-03-14 11:07:40 -0500

    fixed some errors

--- run_all.sh
+++ run_all.sh
@@ -6,15 +6,15 @@ pkill -f main.py # la interfaz Python
 sleep 0.5
 
 # Ejecutable C (socket server)
-cd ~/Proyecto_Final/audio_rpi
+cd audio_rpi
 echo "Iniciando servidor C..."
 ./audio_engine & # ejecutable de C
 sleep 0.5        # espera a que el socket se cree
 
 # Activar virtual environment y correr Python GUI
-cd ~/ProyectoFinal/Interfaz
+cd ../Interfaz
 echo "Activando virtual environment..."
-source env/bin/activate # o| venv/bin/activate si usas venv
+source ../env/bin/activate # o| venv/bin/activate si usas venv
 
 echo "Iniciando interfaz..."
 python main.py &
