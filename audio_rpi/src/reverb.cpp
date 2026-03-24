5b389e72fc0f26c9ff8774915925e715e2db33ef
Samir <samirbarce@gmail.com>
2026-03-23 20:55:39 -0500

    got rid of daisysp reverb

--- audio_rpi/src/reverb.cpp
+++ audio_rpi/src/reverb.cpp
@@ -1,7 +1,10 @@
-#include "daisysp.h"
 #include "reverb.h"
+#include <stdlib.h>
 
-static daisysp::ReverbSc g_reverb;
+#define MAX_DELAY 44100  // 1 segundo máx
+
+static float delay_buffer[MAX_DELAY];
+static int write_idx = 0;
 
 extern "C" void Reverb_init(Reverb *rv, float feedback, float lpfreq, float mix)
 {
@@ -9,19 +12,41 @@ extern "C" void Reverb_init(Reverb *rv, float feedback, float lpfreq, float mix)
     rv->lpfreq   = lpfreq;
     rv->mix      = mix;
 
-    g_reverb.Init(44100.f);
-    g_reverb.SetFeedback(feedback);
-    g_reverb.SetLpFreq(lpfreq);
+    for(int i = 0; i < MAX_DELAY; i++)
+        delay_buffer[i] = 0.0f;
+
+    write_idx = 0;
 }
 
 extern "C" float Reverb_process(Reverb *rv, float input)
 {
-    g_reverb.SetFeedback(rv->feedback);
-    g_reverb.SetLpFreq(rv->lpfreq);
+    // delays distintos (en samples)
+    int d1 = 1116;
+    int d2 = 1188;
+    int d3 = 1277;
+    int d4 = 1356;
 
-    float outL, outR;
-    g_reverb.Process(input, input, &outL, &outR);
+    float y = 0.0f;
 
-    float wet = (outL + outR) * 0.5f;
-    return input * (1.f - rv->mix) + wet * rv->mix;
+    int idx1 = (write_idx - d1 + MAX_DELAY) % MAX_DELAY;
+    int idx2 = (write_idx - d2 + MAX_DELAY) % MAX_DELAY;
+    int idx3 = (write_idx - d3 + MAX_DELAY) % MAX_DELAY;
+    int idx4 = (write_idx - d4 + MAX_DELAY) % MAX_DELAY;
+
+    y = (delay_buffer[idx1] +
+         delay_buffer[idx2] +
+         delay_buffer[idx3] +
+         delay_buffer[idx4]) * 0.25f;
+
+    // simple low-pass (muy importante para que no suene feo)
+    static float lp = 0.0f;
+    float alpha = rv->lpfreq / (rv->lpfreq + 44100.0f);
+    lp = alpha * y + (1.0f - alpha) * lp;
+
+    // feedback
+    delay_buffer[write_idx] = input + lp * rv->feedback;
+
+    write_idx = (write_idx + 1) % MAX_DELAY;
+
+    return input * (1.0f - rv->mix) + lp * rv->mix;
 }
